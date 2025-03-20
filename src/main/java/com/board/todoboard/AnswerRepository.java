9ff0425b24a27caf7492365224cd4a41e55be92c
rloJo <lew991216@gmail.com>
2025-03-20 22:23:23 +0900

    FEAT:Repository 생성

---
+++ src/main/java/com/board/todoboard/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.board.todoboard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/board/todoboard/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.board.todoboard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
